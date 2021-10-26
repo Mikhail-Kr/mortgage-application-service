3499da5545dbdbacbd73538d41afb0a2c93c01c2
MKrasnoperov <muxajlu-4@yandex.ru>
2021-10-26 19:14:09 +0400

    init commit

---
+++ src/main/java/ru/mkr/mortgageapplicationservice/MortgageApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.mkr.mortgageapplicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageApplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageApplicationServiceApplication.class, args);
+	}
+
+}
