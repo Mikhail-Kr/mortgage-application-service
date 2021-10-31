729130fcd4a4da5500c8b0ac22084ff5c12469f3
MKrasnoperov <muxajlu-4@yandex.ru>
2021-10-31 16:29:26 +0400

    SpringBootServletInitializer

--- src/main/java/ru/mkr/mortgageapplicationservice/MortgageApplicationServiceApplication.java
+++ src/main/java/ru/mkr/mortgageapplicationservice/MortgageApplicationServiceApplication.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import ru.mkr.mortgageapplicationservice.customer.CustomerRepository;
 
 
@@ -15,7 +16,7 @@ import ru.mkr.mortgageapplicationservice.customer.CustomerRepository;
 		)
 )
 @SpringBootApplication
-public class MortgageApplicationServiceApplication {
+public class MortgageApplicationServiceApplication extends SpringBootServletInitializer {
 
 	private CustomerRepository customerRepository;
 
